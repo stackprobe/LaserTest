62d441d931cf47040986261da4c97fd7982e20fd
stackprobe <stackprobes@gmail.com>
2018-01-04 11:39:33 +0900

    coffee break

--- Release_Debug.bat
+++ Release_Debug.bat
@@ -6,8 +6,10 @@ MD out\res
 
 COPY /B Project\Release\LaserTest.exe out\bin\bin
 
-C:\Factory\DevTools\SemiRelease.exe Music.txt       out out\res\Music_       ..\..\res\Music_
-C:\Factory\DevTools\SemiRelease.exe Picture.txt     out out\res\Picture_     ..\..\res\Picture_
-C:\Factory\DevTools\SemiRelease.exe SoundEffect.txt out out\res\SoundEffect_ ..\..\res\SoundEffect_
+C:\Factory\DevTools\SemiRelease.exe Music.txt       out out\res\Music_       res\Music_
+C:\Factory\DevTools\SemiRelease.exe Picture.txt     out out\res\Picture_     res\Picture_
+C:\Factory\DevTools\SemiRelease.exe SoundEffect.txt out out\res\SoundEffect_ res\SoundEffect_
+
+> out\_Boot.bat ECHO START /Dbin\bin LaserTest.exe
 
 PAUSE
